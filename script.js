392e1bf72ae55fa808f0d60f96a39181a484f16f
Kartikey <Kartikeyjoshi12@gmail.com>
2022-05-30 23:31:59 +0530

    Working Calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let displayValue = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      displayValue = eval(displayValue);
+      document.getElementById("display").value = displayValue;
+      displayValue = ""
+    }
+    else if(e.target.innerHTML == 'AC'){
+      displayValue = ""
+      document.getElementById("display").value = displayValue;
+    }
+    else{ 
+    displayValue = displayValue + e.target.innerHTML;
+    document.getElementById("display").value = displayValue;
+      }
+  })
+})
+
+const backspaceBtn = document.getElementById('backspace')
+
+function erase(){
+    display = display.split(" ")
+}
